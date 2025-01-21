85bb3ee76c54117f11997dde0a11bd108cb74179
Stephan Lipp <sphlpp91@gmail.com>
2025-01-22 00:43:27 +0100

    feat: merge multiple summary files

--- scripts/crash-dedup/src/cdd/main.py
+++ scripts/crash-dedup/src/cdd/main.py
@@ -14,6 +14,7 @@
 
 import logging
 import sys
+from itertools import chain
 from pathlib import Path
 from typing import List, Optional
 
@@ -22,6 +23,7 @@ from typing_extensions import Annotated
 
 from cdd import AppConfig
 from cdd.container.san import SanitizerOutput
+from cdd.container.summary import DedupSummary
 from cdd.grouping import group_by
 from cdd.utils.fs import find_files
 from cdd.utils.proc import get_cpu_count, run_program_with_sanitizer, run_with_multiproc
@@ -147,7 +149,7 @@ def run(
 
     if len(sanitizer_files) == 0:
         logging.info("No sanitizer output files found.")
-        exit(0)
+        exit(1)
 
     sanitizer_infos = []
     for sanitizer_file in sanitizer_files:
@@ -162,3 +164,69 @@ def run(
 
         summary = group_by(sanitizer_infos, n_frames, consider_filepaths, consider_lines)
         summary.to_csv(summary_file)
+
+
+@app.command(help="Merge multiple summary files.")
+def merge(
+    input_files: Annotated[
+        List[Path],
+        typer.Option(
+            "--input",
+            writable=False,
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            resolve_path=True,
+            help="Path to the input summary file.",
+        ),
+    ],
+    output_file: Annotated[
+        Path,
+        typer.Option(
+            "--output",
+            writable=True,
+            exists=False,
+            file_okay=True,
+            dir_okay=False,
+            resolve_path=True,
+            help="Path to the output summary file.",
+        ),
+    ],
+    n_frames: Annotated[
+        Optional[int],
+        typer.Option(
+            "--frames",
+            min=1,
+            help="Number(s) of stack frames to be included in deduplication. Note: If not specified, all frames are considered.",
+        ),
+    ] = None,
+    consider_filepaths: Annotated[
+        bool,
+        typer.Option(
+            "--consider-filepaths",
+            is_flag=True,
+            help="Consider the filepaths within the stack frames for deduplication.",
+        ),
+    ] = False,
+    consider_lines: Annotated[
+        bool,
+        typer.Option(
+            "--consider-lines",
+            is_flag=True,
+            help="Consider the line numbers within the stack frames for deduplication.",
+        ),
+    ] = False,
+) -> None:
+    def flatten(l: List) -> List:
+        return list(chain.from_iterable(l))
+
+    try:
+        sanitizer_infos = flatten(
+            [entry.elems for entry in flatten([DedupSummary.from_csv(file).summary for file in input_files])]
+        )
+
+        summary = group_by(sanitizer_infos, n_frames, consider_filepaths, consider_lines)
+        summary.to_csv(output_file)
+    except Exception as ex:
+        logging.error(ex)
+        exit(1)
